54f2d70fab435e7f130a1e60e8bc78dcdde3797c
khushmohammad <khushbhaijaan007@gmail.com>
2022-01-26 18:29:50 +0400

    ok
    ok

--- src/component/content/homepage/Homepage.js
+++ src/component/content/homepage/Homepage.js
@@ -11,7 +11,7 @@ function Homepage() {
     return (
       <section className="Homepage">
         <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-          <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+          <Alert.Heading>Oh snap! You got an error! alert</Alert.Heading>
           <p>
             Change this and that and try again. Duis mollis, est non commodo
             luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
